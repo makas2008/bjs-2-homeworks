c7bb1e16f80c1f8fa63c2b71bc33a3e8a4e81deb
Николай <rakach2009@mail.ru>
2025-01-09 22:31:09 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,25 +10,23 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (this.hasOwnProperty(`marks`) === true) {
-    this.marks.push(...marks);
-  }
+  if (!this.marks) {
+        console.log("Student is excluded");
+        return;
+    } 
 }
 
 Student.prototype.getAverage = function () {
-  if (this.hasOwnProperty(`marks`) === false || this.marks.length === 0) {
-    return 0;
-  }
-    if (index === arr.length - 1) {
-        return acc / arr.length;
-    }
-    return acc;
-    }, 0);
-    return gradePointAverage;
+  //return this.marks.reduce((acc, mark, index, arr) => acc + mark / arr.length, 0);
+  if (!this.marks) {
+       return 0;
+   } else {
+       return this.marks.reduce((acc, mark, index, arr) => acc + mark / arr.length, 0); 
+   }
 }
 
 Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason; 
 }
